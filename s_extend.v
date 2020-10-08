d9d6c5d245ed1a0f496d0b2fed2f7e03201aafba
Alan Leonel Perez <57515968+alanlperez@users.noreply.github.com>
2020-10-07 20:45:24 -0700

    Add files via upload

---
+++ s_extend.v
@@ -0,0 +1,21 @@
+// Class: CSE 4010 Computer Architecture
+// Term: Fall 2020
+// Name(s): Alan Leonel Perez ID: 004862867
+//
+// Lab #2
+`timescale 1ns / 1ps
+
+/*  s_extend.v  */
+
+module s_extend(
+    input	wire	[15:0] nextend,
+    output	reg	[31:0] extend
+    );
+
+	always@ * 
+	  begin
+		// Replicate signed bit 16 times then cancatinate
+		extend <= {{16{nextend[15]}}, nextend[15:0]};
+		end
+
+endmodule
